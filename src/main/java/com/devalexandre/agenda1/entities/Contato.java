f032102a39eca762287f0e531c2457b0b1a193c8
alex241262 <alexandreferreira168@gmail.com>
2023-08-01 00:22:50 -0400

    Criado classe Contato

---
+++ src/main/java/com/devalexandre/agenda1/entities/Contato.java
@@ -0,0 +1,56 @@
+package com.devalexandre.agenda1.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_contato")
+public class Contato {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String fone;
+	private String celular;
+	
+	public Contato() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	
+}
